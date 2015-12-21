5d9edf53912458b4cb37ad9836b5c01ae89d093b
andre96125 <andre.colo222@gmail.com>
2015-12-21 13:53:43 +0100

    aggiunta programma esistente

---
+++ programma con classi/condivisi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programma_con_classi
+{
+    class condivisi
+    {
+        static List<String> ListaAccount = new List<String>();
+        static public void aggiungi(string dati)
+        {
+            ListaAccount.Add(dati);
+        }
+
+    }
+}
---
+++ programma con classi/thread.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programma_con_classi
+{
+    class thread
+    {
+        StreamReader sr;
+        StreamWriter sw;
+        public thread(TcpClient c)
+        {
+            sr = new StreamReader(c.GetStream());
+            sw = new StreamWriter(c.GetStream());
+        } 
+
+        public void inserisci()
+        {
+            sw.WriteLine("invia");
+            sw.Flush();
+            String s = sr.ReadLine();
+            condivisi.aggiungi(s);
+        }
+    }
+}
